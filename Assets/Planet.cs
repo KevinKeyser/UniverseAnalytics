7011270629222da133859529f04486e1430e0791
Kevin Keyser <kevin.keyser@live.com>
2017-04-23 00:50:43 -0700

    Rendering Planet Locations and Updated CSV

--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -11,33 +11,58 @@ public enum Status
     Accepted
 }
 
-public class Planet : ScriptableObject {
+[Serializable]
+public class Planets
+{
+    [SerializeField]
+    private Planet[] planets;
+    public Planet this[int index]
+    {
+        get { return planets[index]; }
+        set { planets[index] = value; }
+    }
+
+    public int Count { get { return planets.Length; } }
+}
+
+[Serializable]
+public class Planet {
     private static readonly double earthMass = 5.97237 * Math.Pow(10, 24);
     private static readonly float kmEarthRadii = 6371.0f;
 
-    public string Name;
-    private double mass;
-    public double Mass
-    {
-        get
-        {
-            return earthMass * mass;
-        }
-    }
+    [SerializeField]
+    private string pl_name;
+    public string Name { get { return pl_name; } }
 
-    private float radius;
-    public float Radius
-    {
-        get
-        {
-            return Radius * kmEarthRadii;
-        }
-    }
+    [SerializeField]
+    private string pl_masse;
+    public double Mass { get { return String.IsNullOrEmpty(pl_masse) ? 0 : earthMass * double.Parse(pl_masse); } }
 
-    public float RightAscension;
-    public float Declination;
-    public float PcDistance;
-    public int DiscoveryYear;
-    public int Moons;
-    public Status Status;
+    [SerializeField]
+    private string pl_rade;
+    public float Radius { get { return String.IsNullOrEmpty(pl_rade) ? 0 : kmEarthRadii * float.Parse(pl_rade); } } 
+
+    [SerializeField]
+    private string ra;
+    public float RightAscension { get { return String.IsNullOrEmpty(ra) ? 0 : float.Parse(ra) - 180; } }
+
+    [SerializeField]
+    private string dec;
+    public float Declination { get { return String.IsNullOrEmpty(dec) ? 0 : float.Parse(dec); } }
+
+    [SerializeField]
+    private string st_dist;
+    public float PcDistance { get { return String.IsNullOrEmpty(st_dist) ? 0 : float.Parse(st_dist); } }
+
+    [SerializeField]
+    private string pl_disc;
+    public int DiscoveryYear { get { return String.IsNullOrEmpty(pl_disc) ? 0 : int.Parse(pl_disc); } }
+
+    [SerializeField]
+    private string pl_mnum;
+    public int Moons { get { return String.IsNullOrEmpty(pl_mnum) ? 0 : int.Parse(pl_mnum); } }
+
+    [SerializeField]
+    private string pl_status;
+    public Status Status { get { return String.IsNullOrEmpty(pl_status) ? Status.Retracted : (Status)(int.Parse(pl_status)); } }
 }
---
+++ Assets/Universe.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class PlanetInformation : MonoBehaviour
+{
+    public Planet Information;
+}
+
+public class Universe : MonoBehaviour
+{
+    public string FileLocation;
+    public Mesh Mesh;
+    public Material Material;
+    [HideInInspector]
+    public Planets Planets;
+
+    public void Awake()
+    {
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+        if (File.Exists(FileLocation))
+        {
+            string contents = File.ReadAllText(FileLocation);
+            Planets = JsonUtility.FromJson<Planets>(contents);
+            sw.Stop();
+            Debug.Log(sw.ElapsedMilliseconds);
+        }
+        for (int i = 0; i < Planets.Count; i++)
+        {
+            GameObject planet = new GameObject(Planets[i].Name);
+            planet.AddComponent<MeshFilter>().sharedMesh = Mesh;
+            planet.AddComponent<MeshRenderer>().sharedMaterial = Material;
+            planet.AddComponent<PlanetInformation>().Information = Planets[i];
+            planet.transform.SetParent(transform);
+            planet.transform.position = Planets[i].PcDistance * PolarToCartesian(new Vector2(Planets[i].Declination, Planets[i].RightAscension));
+        }
+    }
+
+    Vector3 PolarToCartesian(Vector2 polar)
+    {
+        //an origin vector, representing lat,lon of 0,0. 
+        Vector3 origin = new Vector3(0, 0, 1);
+        //build a quaternion using euler angles for lat,lon
+        Quaternion rotation = Quaternion.Euler(polar.x, polar.y, 0);
+        //transform our reference vector by the rotation. Easy-peasy!
+        Vector3 point = rotation * origin;
+        return point;
+    }
+}
